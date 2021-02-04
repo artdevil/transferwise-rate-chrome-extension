33170023433f2b9674ac9beb3ce748e04805d203
adhiguna <adhiguna.sabril@gmail.com>
2021-02-04 21:53:22 +0700

    keep build showing

---
+++ build/popup.js
@@ -0,0 +1,228 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/popup.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/popup.css":
+/*!***********************!*\
+  !*** ./src/popup.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/popup.js":
+/*!**********************!*\
+  !*** ./src/popup.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _popup_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./popup.css */ "./src/popup.css");
+/* harmony import */ var _popup_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_popup_css__WEBPACK_IMPORTED_MODULE_0__);
+
+
+
+
+(function() {
+  // We will make use of Storage API to get and store `count` value
+  // More information on Storage API can we found at
+  // https://developer.chrome.com/extensions/storage
+
+  // To get storage access, we have to mention it in `permissions` property of manifest.json file
+  // More information on Permissions can we found at
+  // https://developer.chrome.com/extensions/declare_permissions
+  const counterStorage = {
+    get: cb => {
+      chrome.storage.sync.get(['count'], result => {
+        cb(result.count);
+      });
+    },
+    set: (value, cb) => {
+      chrome.storage.sync.set(
+        {
+          count: value,
+        },
+        () => {
+          cb();
+        }
+      );
+    },
+  };
+
+  function setupCounter(initialValue = 0) {
+    document.getElementById('counter').innerHTML = initialValue;
+
+    document.getElementById('incrementBtn').addEventListener('click', () => {
+      updateCounter({
+        type: 'INCREMENT',
+      });
+    });
+
+    document.getElementById('decrementBtn').addEventListener('click', () => {
+      updateCounter({
+        type: 'DECREMENT',
+      });
+    });
+  }
+
+  function updateCounter({ type }) {
+    counterStorage.get(count => {
+      let newCount;
+
+      if (type === 'INCREMENT') {
+        newCount = count + 1;
+      } else if (type === 'DECREMENT') {
+        newCount = count - 1;
+      } else {
+        newCount = count;
+      }
+
+      counterStorage.set(newCount, () => {
+        document.getElementById('counter').innerHTML = newCount;
+
+        // Communicate with content script of
+        // active tab by sending a message
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+          const tab = tabs[0];
+
+          chrome.tabs.sendMessage(
+            tab.id,
+            {
+              type: 'COUNT',
+              payload: {
+                count: newCount,
+              },
+            },
+            response => {
+              console.log('Current count value passed to contentScript file');
+            }
+          );
+        });
+      });
+    });
+  }
+
+  function restoreCounter() {
+    // Restore count value
+    counterStorage.get(count => {
+      if (typeof count === 'undefined') {
+        // Set counter value as 0
+        counterStorage.set(0, () => {
+          setupCounter(0);
+        });
+      } else {
+        setupCounter(count);
+      }
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', restoreCounter);
+
+  // Communicate with background file by sending a message
+  chrome.runtime.sendMessage(
+    {
+      type: 'GREETINGS',
+      payload: {
+        message: 'Hello, my name is Pop. I am from Popup.',
+      },
+    },
+    response => {
+      console.log(response.message);
+    }
+  );
+})();
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=popup.js.map
