55de4dca4fe1247e433403d7d6126173dff04429
adhiguna <adhiguna.sabril@gmail.com>
2021-02-04 21:32:19 +0700

    update init

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # <img src="public/icons/icon_48.png" width="45" align="left"> Transferwise Rate
 
-My Chrome Extension
-
-## Features
-
-- Feature 1
-- Feature 2
+This is unofficial extension for automatically get rate from transferwise. only support from USD to IDR, other rate coming soon !
 
 ## Install
-
-[**Chrome** extension]() <!-- TODO: Add chrome extension link inside parenthesis -->
-
-## Contribution
-
-Suggestions and pull requests are welcomed!.
+1. Download project
+2. Open chrome://extensions
+3. Check the Developer mode checkbox
+4. Click on the Load unpacked extension button
+5. Select the folder transferwise-rate/build
 
--- src/background.js
+++ src/background.js
@@ -1,21 +1,52 @@
 'use strict';
+const scrapeIt = require("scrape-it")
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.type === 'GREETINGS') {
+//     const message = `Hi ${
+//       sender.tab ? 'Con' : 'Pop'
+//     }, my name is Bac. I am from Background. It's great to hear from you.`;
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+//     // Log message coming from the `request` parameter
+//     console.log(request.payload.message);
+//     // Send a response message
+//     sendResponse({
+//       message,
+//     });
+//   }
+// });
+
+chrome.runtime.onInstalled.addListener(function() {
+  let times = 60 * 1000
+
+  setBadge()
+
+  setInterval(function(){
+    setBadge()
+    console.log('hello')
+  }, times);
+
 });
+
+async function setBadge() {
+  let text = await searchRate()
+  chrome.browserAction.setBadgeText({text: text});
+}
+
+function searchRate () {
+  return new Promise(resolve => {
+    scrapeIt("https://transferwise.com/gb/currency-converter/usd-to-idr-rate", {
+      rate: "#calculator > form > div.row.cc-calculator__input-group.m-t-3 > div.col-lg-6.text-xs-center.text-lg-left > h3 > span.text-success"
+    }).then(({ data }) => {
+      resolve(Math.floor(data.rate).toString())
+    }).catch((err) => {
+      console.error(err.message)
+      resolve('Err')
+    })
+  });
+}
