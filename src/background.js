a3e71c7ef31ed011bf8019ba477faec58aaed0d7
adhiguna <adhiguna.sabril@gmail.com>
2021-02-06 09:19:02 +0700

    update for popup

--- src/background.js
+++ src/background.js
@@ -5,6 +5,22 @@ const cron = require('node-cron');
 var currencyFrom = 'usd'
 var currencyTo = 'idr'
 
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse){
+    if(message == undefined || Object.keys(message).length == 0) return;
+
+    if (message.type === "UPDATE_CURRENCY") {
+      chrome.browserAction.setBadgeText({text: ''});
+      setBadge()
+    }
+
+    // Send an empty response
+    // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
+    sendResponse({});
+    return true;
+  }
+);
+
 chrome.runtime.onInstalled.addListener(function() {
   setBadge()
 
@@ -13,28 +29,13 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-chrome.browserAction.onClicked.addListener(function(_tab) {
-  window.open(getUrl());
-});
-
 async function setBadge() {
+  await setCurrency();
   let text = await searchRate();
   chrome.browserAction.setBadgeText({text: text});
 }
 
 function searchRate () {
-  chrome.storage.local.get('currency', function(result) {
-    if (result.key) {
-      if (result.key.currency_from) {
-        currencyFrom = result.key.currency_from
-      }
-
-      if (result.key.currency_to) {
-        currencyTo = result.key.currency_to
-      }
-    }
-  });
-
   return new Promise(resolve => {
     scrapeIt(getUrl(), {
       rate: "#calculator > form > div.row.cc-calculator__input-group.m-t-3 > div.col-lg-6.text-xs-center.text-lg-left > h3 > span.text-success"
@@ -44,6 +45,24 @@ function searchRate () {
       console.error(err.message)
       resolve('Err')
     })
+  });
+}
+
+function setCurrency () {
+  return new Promise(resolve => {
+    chrome.storage.local.get('currency', function(result) {
+      if (result.currency) {
+        if (result.currency.currency_from) {
+          currencyFrom = result.currency.currency_from
+        }
+
+        if (result.currency.currency_to) {
+          currencyTo = result.currency.currency_to
+        }
+
+        resolve(true)
+      }
+    });
   });
 }
 
--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,263 @@
 'use strict';
 
 import './popup.css';
+import 'choices.js/public/assets/styles/choices.css';
+import Choices from 'choices.js';
 
 (function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
+  var currencyFrom = 'usd'
+  var currencyTo = 'idr'
 
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
+  var listCountry = [
+    "EUR",
+    "USD",
+    "GBP",
+    "INR",
+    "CAD",
+    "AUD",
+    "CHF",
+    "MXN",
+    "FJD",
+    "SCR",
+    "BBD",
+    "GTQ",
+    "CLP",
+    "HNL",
+    "UGX",
+    "ZAR",
+    "TND",
+    "BSD",
+    "SLL",
+    "GMD",
+    "TWD",
+    "RSD",
+    "DOP",
+    "KMF",
+    "MYR",
+    "FKP",
+    "XOF",
+    "GEL",
+    "UYU",
+    "MAD",
+    "CVE",
+    "TOP",
+    "AZN",
+    "OMR",
+    "PGK",
+    "KES",
+    "SEK",
+    "BTN",
+    "UAH",
+    "GNF",
+    "MZN",
+    "SVC",
+    "ARS",
+    "QAR",
+    "CNY",
+    "THB",
+    "UZS",
+    "XPF",
+    "MRU",
+    "BDT",
+    "BMD",
+    "KWD",
+    "PHP",
+    "RUB",
+    "PYG",
+    "ISK",
+    "JMD",
+    "COP",
+    "MKD",
+    "DZD",
+    "PAB",
+    "GGP",
+    "SGD",
+    "ETB",
+    "JEP",
+    "KGS",
+    "VUV",
+    "LAK",
+    "BND",
+    "XAF",
+    "LRD",
+    "HRK",
+    "ALL",
+    "DJF",
+    "ZMW",
+    "TZS",
+    "VND",
+    "ILS",
+    "GHS",
+    "GYD",
+    "BOB",
+    "KHR",
+    "MDL",
+    "IDR",
+    "KYD",
+    "AMD",
+    "BWP",
+    "SHP",
+    "TRY",
+    "LBP",
+    "TJS",
+    "JOD",
+    "AED",
+    "HKD",
+    "RWF",
+    "LSL",
+    "DKK",
+    "BGN",
+    "MMK",
+    "MUR",
+    "NOK",
+    "IMP",
+    "GIP",
+    "RON",
+    "LKR",
+    "NGN",
+    "CRC",
+    "CZK",
+    "PKR",
+    "XCD",
+    "ANG",
+    "HTG",
+    "BHD",
+    "KZT",
+    "SRD",
+    "SZL",
+    "SAR",
+    "TTD",
+    "MVR",
+    "AWG",
+    "KRW",
+    "NPR",
+    "JPY",
+    "MNT",
+    "AOA",
+    "PLN",
+    "SBD",
+    "BYN",
+    "HUF",
+    "MWK",
+    "MGA",
+    "BZD",
+    "BAM",
+    "EGP",
+    "MOP",
+    "NAD",
+    "NIO",
+    "PEN",
+    "NZD",
+    "WST",
+    "TMT",
+    "BRL"
+  ]
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
+  function setDropDown () {
+    for (const element of listCountry) {
+      let option = document.createElement('option');
+      option.value = element.toLowerCase()
+      option.innerHTML = element
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
+      let cloneOption = option.cloneNode(true)
 
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
+      getCurrencyFrom().appendChild(option)
+      getCurrencyTo().appendChild(cloneOption)
     }
-  );
+  }
+
+  async function setValueDropDown () {
+    let result = await getCurrency()
+
+    if (result && result.currency) {
+      if (result.currency.currency_from) {
+        currencyFrom = result.currency.currency_from
+        getCurrencyFrom().value = currencyFrom
+      }
+
+      if (result.currency.currency_to) {
+        currencyTo = result.currency.currency_to
+        getCurrencyTo().value = currencyTo
+      }
+    }
+
+    setChoiceLib()
+  }
+
+  function getCurrencyFrom() {
+    return document.getElementById('currency_from')
+  }
+
+  function getCurrencyTo() {
+    return document.getElementById('currency_to')
+  }
+
+  function setChoiceLib () {
+    new Choices(getCurrencyFrom(), {
+      searchResultLimit: 1,
+      position: 'bottom',
+      renderChoiceLimit: 1
+    })
+
+    getCurrencyFrom().addEventListener(
+      'choice',
+      function(event) {
+        console.log(event)
+        currencyFrom = event.detail.choice.value
+      },
+      false,
+    );
+
+    new Choices(getCurrencyTo(), {
+      searchResultLimit: 1,
+      position: 'bottom',
+      renderChoiceLimit: 1
+    })
+
+    getCurrencyTo().addEventListener(
+      'choice',
+      function(event) {
+        currencyTo = event.detail.choice.value
+      },
+      false,
+    );
+  }
+
+  function getCurrency () {
+    return new Promise(resolve => {
+      chrome.storage.local.get('currency', function(result) {
+        resolve(result)
+      });
+    })
+  }
+
+  function setStorage () {
+    chrome.storage.local.set({
+      currency: {
+        currency_from: getCurrencyFrom().value,
+        currency_to: getCurrencyTo().value,
+      }
+    }, sendMessageToBackground())
+  }
+
+  function sendMessageToBackground () {
+    chrome.runtime.sendMessage({type: "UPDATE_CURRENCY"}, function(_response) {});
+  }
+
+  function getUrl() {
+    return `https://transferwise.com/gb/currency-converter/${currencyFrom}-to-${currencyTo}-rate`
+  }
+
+  setDropDown()
+  setValueDropDown()
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('apply').addEventListener('click', function() {
+      setStorage()
+    });
+
+    document.getElementById('check-link').addEventListener('click', function() {
+      window.open(getUrl());
+    });
+  });
 })();
